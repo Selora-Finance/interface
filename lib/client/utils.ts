42fdcf08827e25dd3508186716b830096d05b682
Kingsley Victor <javaprodigy56@gmail.com>
2025-10-19 20:32:59 +0100

    chore: made updates

--- lib/client/utils.ts
+++ lib/client/utils.ts
@@ -7,7 +7,7 @@ import { twMerge } from 'tailwind-merge';
  * cn - className helper
  * Combines multiple class strings, removes duplicates, and merges Tailwind classes.
  */
-export function cn(...inputs: string[]) {
+export function cn(...inputs: any[]) {
   return twMerge(clsx(inputs));
 }
 
