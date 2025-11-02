fe198c9948e81506f2701fcd5c968dd4eb064e7d
Kingsley Victor <javaprodigy56@gmail.com>
2025-11-02 01:09:30 +0100

    chore: add suspense

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import { AppView } from './__render__';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Selora Finance | Central liquidity & trading marketplace on Fluent',
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AppView>{children}</AppView>
+      <Suspense>
+        <AppView>{children}</AppView>
+      </Suspense>
     </html>
   );
 }
